b5b9cf7c1b23c82e4175cde1cda31f5ac60cb2ab
v̵͎̄̇̐̌̍ā̸̳̺̯̣͎̱͂̏̕̕̚ͅc̴̡̢̘̪̥͊͋̇̑́̓̑͜͜ͅu̵̗͈̭̜͓̮͎͒̀̀ự̴̑̏͊͂̾m̷̲̱͇̰̥̬̫̏̔̍͒͌̈́̒̍͑̕͜͝ <21699296+v-a-c-u-u-m@users.noreply.github.com>
2021-08-15 04:30:06 -0700

    v0.1.9

--- src/shellcode64.s
+++ src/shellcode64.s
@@ -3,14 +3,24 @@ BITS 64
 global _start
 
 _start:
-    push rax
-    push rbx
-    push rcx
-    push rdx
-    push rdi
-    push rsi
-    push rbp
+    push rax ;[rsp+120] ; empty
+    push rbp ;[rsp+112]
+    push r8  ;[rsp+104]
+    push r9  ;[rsp+96]
+    push r10 ;[rsp+88]
+    push r11 ;[rsp+80]
+    push r12 ;[rsp+72]
+    push r13 ;[rsp+64]
+    push r14 ;[rsp+56]
+    push r15 ;[rsp+48]
+    push rax ;[rsp+40]
+    push rbx ;[rsp+32]
+    push rcx ;[rsp+24]
+    push rdx ;[rsp+16]
+    push rsi ;[rsp+8]
+    push rdi ;[rsp]
     jmp short _trick
+    ;jmp short _restore
 
 _code:
     xor rax, rax
@@ -45,16 +55,26 @@ _dlsym:
     call rcx
     test rax, rax
     je _restore
+    mov rdi, [rsp] ; current rdi <- stack rdi
     call rax
 
 _restore:
-    pop rbp
-    pop rsi
     pop rdi
+    pop rsi
     pop rdx
     pop rcx
     pop rbx
     pop rax
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rbp
+    add rsp, 8
 
 _ret:
     ret
@@ -62,3 +82,4 @@ _ret:
 _trick:
     call _code
 
+; name="shellcode64"; nasm $name.s -o $name.payload; nasm -f elf64 $name.s -o $name.o; ld $name.o -o $name; hexdump -C $name.payload; echo -e "\n\n"; ndisasm -b 64 $name.payload && ./bin_to_c.py $name.payload
