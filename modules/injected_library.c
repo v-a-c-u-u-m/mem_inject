067b0c0a7bb3f1e8d23594a2b5c044c4c1b9c04d
v̵͎̄̇̐̌̍ā̸̳̺̯̣͎̱͂̏̕̕̚ͅc̴̡̢̘̪̥͊͋̇̑́̓̑͜͜ͅu̵̗͈̭̜͓̮͎͒̀̀ự̴̑̏͊͂̾m̷̲̱͇̰̥̬̫̏̔̍͒͌̈́̒̍͑̕͜͝ <21699296+v-a-c-u-u-m@users.noreply.github.com>
2021-08-15 04:31:13 -0700

    v0.1.9

---
+++ modules/injected_library.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+
+int entry_point() {
+    puts("                            ");
+    puts("you");
+    puts("have been");
+    puts("hacked");
+    puts("by lib");
+    puts("!!!");
+    return 0;
+}
