0686ff50ea08777b291e0725743220adbefa926f
v̵͎̄̇̐̌̍ā̸̳̺̯̣͎̱͂̏̕̕̚ͅc̴̡̢̘̪̥͊͋̇̑́̓̑͜͜ͅu̵̗͈̭̜͓̮͎͒̀̀ự̴̑̏͊͂̾m̷̲̱͇̰̥̬̫̏̔̍͒͌̈́̒̍͑̕͜͝ <21699296+v-a-c-u-u-m@users.noreply.github.com>
2021-08-15 04:25:56 -0700

    v0.1.9

--- src/hello.c
+++ src/hello.c
@@ -9,10 +9,34 @@ int test() {
     return 0;
 }
 
+int ret1() {
+    return 0;
+};
+
+int ret2() {
+    return 0;
+};
+
+int f1(int i) {
+    fprintf(stderr, "Round[f1] %d\n", i);
+    ret1();
+    return 0;
+}
+
+int f2(int i) {
+    fprintf(stderr, "Round[f2] %d\n", i);
+    ret2();
+    return 0;
+}
+
 int loop(unsigned int times, unsigned int delay) {
     char s[] = "hello world!";
     for (int i = 1; i < times; i++) {
-        fprintf(stderr, "Round %d\r", i);
+        if (i % 2 == 0) {
+            f1(i);
+        } else {
+            f2(i);
+        }
         test();
         //printf("\n");
         sleep(2);
